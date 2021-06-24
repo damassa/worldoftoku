5ffbccb5947d3fc59cd59c730ff60a85d5fb4361
Felipe Leal Damasceno <felipelealdamasceno@gmail.com>
2021-06-23 21:46:27 -0300

    Data fetched by serie id

--- server/src/models/Serie.js
+++ server/src/models/Serie.js
@@ -1,33 +1,39 @@
 const mongoose = require('mongoose');
 
-const SerieSchema = new mongoose.Schema({
+const SerieSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     plot: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     image: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     opening_video: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     year: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     duration: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
-    category: [{
-        type: mongoose.Schema.Types.ObjectId, ref: 'Category'
-    }],
-}, { timestamps: true, collection: 'series' });
+    category: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+      },
+    ],
+  },
+  { timestamps: true, collection: 'series' },
+);
 
 module.exports = mongoose.model('Serie', SerieSchema);
--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,61 +1,61 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { Grid } from '@material-ui/core';
 import api from '../../services/api';
-import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player/youtube';
 
 import useStyles from './styles';
 
 const Detail = () => {
   const classes = useStyles();
-  const [series, setSeries] = useState([]);
+  const [data, setData] = useState([]);
+  const { id } = useParams();
 
   useEffect(() => {
     api
-      .get('series')
+      .get(`series/${id}`)
       .then((response) => {
-        setSeries(response.series);
+        setData(response.data);
         console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [id]);
 
   return (
     <Grid container className={classes.DetailWrapper}>
       <Grid item xs={12}>
         <Grid container justify="center">
           <Grid item xs={10}>
-            {series.map((serie) => (
-              <Grid key={serie._id} container className={classes.DetailContent}>
-                <Grid item md={4}>
-                  <Grid container className={classes.DetailImage}>
-                    <Grid item sm={12} md={8}>
-                      <img src={serie.image} alt="Detalhe" title={serie.name} />
-                    </Grid>
-                  </Grid>
-                  <Grid container className={classes.SerieDetail}>
-                    <Grid item sm={12} md={8}>
-                      <h1>{serie.name}</h1>
-                      <h2>{serie.year}</h2>
-                      <h3>{serie.duration}</h3>
-                      <Grid item xs={12} className={classes.CategoryTag}>
-                        <div>{serie.category}</div>
-                      </Grid>
-                    </Grid>
+            <Grid container className={classes.DetailContent}>
+              <Grid item md={4}>
+                <Grid container className={classes.DetailImage}>
+                  <Grid item sm={12} md={8}>
+                    <img src={data.image} alt="Detalhe" title={data.name} />
                   </Grid>
                 </Grid>
-                <Grid item sm={12} md={8}>
-                  <Grid item xs={12} className={classes.OpeningVideo}>
-                    <ReactPlayer width="100%" url={serie.opening_video} />
-                  </Grid>
-                  <Grid item xs={12} className={classes.SeriePlot}>
-                    <h2>Sinopse</h2>
-                    <p>{serie.plot}</p>
+                <Grid container className={classes.SerieDetail}>
+                  <Grid item sm={12} md={8}>
+                    <h1>{data.name}</h1>
+                    <h2>{data.year}</h2>
+                    <h4>{data.duration} minutes</h4>
+                    <Grid item xs={12} className={classes.CategoryTag}>
+                      <div>{data.category}</div>
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-            ))}
+              <Grid item sm={12} md={8}>
+                <Grid item xs={12} className={classes.OpeningVideo}>
+                  <ReactPlayer width="100%" url={data.opening_video} />
+                </Grid>
+                <Grid item xs={12} className={classes.SeriePlot}>
+                  <h2>Sinopse</h2>
+                  <p>{data.plot}</p>
+                </Grid>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>

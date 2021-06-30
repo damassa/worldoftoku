9925a8e02d42a63e188d5d01d226012e330be017
Felipe Leal Damasceno <felipelealdamasceno@gmail.com>
2021-06-30 20:10:55 -0300

    Component removed and working on login and alter data from user.

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import FormUser from '../../components/formUser';
-
-import { Grid } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../services/api';
+import { Grid, TextField } from '@material-ui/core';
 
 import useStyles from './styles';
 
 function User() {
   const classes = useStyles();
+  const [data, setData] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    api
+      .get(`users/${id}`)
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
 
   return (
     <Grid container className={classes.UserWrapper}>
@@ -18,7 +32,68 @@ function User() {
                 <h1>Altere dados da sua conta!</h1>
               </Grid>
               <Grid item xs={12}>
-                <FormUser />
+                <Grid container justify="center">
+                  <Grid item xs={12}>
+                    <TextField
+                      inputProps={{ maxLenght: 25 }}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="user"
+                      label="Usuário:"
+                      type="text"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      inputProps={{ maxLenght: 25 }}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="email"
+                      label="E-mail:"
+                      type="text"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      inputProps={{ maxLenght: 25 }}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="senha"
+                      label="Senha atual:"
+                      type="text"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      inputProps={{ maxLenght: 25 }}
+                      variant="outlined"
+                      fullWidth
+                      name="senha"
+                      label="Nova senha:"
+                      type="text"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      inputProps={{ maxLenght: 25 }}
+                      variant="outlined"
+                      fullWidth
+                      name="repeatSenha"
+                      label="Confirme a nova senha:"
+                      type="text"
+                    />
+                  </Grid>
+                  <Grid container spacing={3} justify="flex-start">
+                    <Grid className={classes.formButton} item xs={12}>
+                      <button type="submit" disabled={false}>
+                        Alterar
+                      </button>
+                    </Grid>
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
--- src/pages/User/styles.js
+++ src/pages/User/styles.js
@@ -1,8 +1,29 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   UserWrapper: {
     padding: '20px 0 50px 0',
+  },
+  formButton: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    [theme.breakpoints.down('md')]: {
+      justifyContent: 'center',
+    },
+    '& button': {
+      padding: '10px 20px',
+      borderRadius: 10,
+      cursor: 'pointer',
+      marginTop: 20,
+      border: 'none',
+      backgroundColor: '#66569a',
+      transition: '.2s',
+      color: '#FAEAFF',
+      '&:hover': {
+        backgroundColor: '#6e5da8',
+        transition: '.2s',
+      },
+    },
   },
 }));
 
